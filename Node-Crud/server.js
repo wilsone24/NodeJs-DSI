7317b3374dbbccc4ddb22883bc7df7efb2dddf49
wilsone24 <wilsone@uninorte.edu.co>
2025-03-21 08:51:15 -0500

    Rename contact route module for consistency

--- Node-Crud/server.js
+++ Node-Crud/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const contactsRouter = require("./routes/contact");
+const contactsRouter = require("./routes/contactRoute");
 
 const app = express();
 const port = process.env.PORT;
